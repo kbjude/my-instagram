c3c3bd64a437337e5ca5862c9a4b19900f0071b6
kbjude <kbjude@gmail.com>
2022-11-06 17:06:16 +0300

    Working with functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = 'John Doe'
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello {name}
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Button.jsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <Button onClick = {() => alert('hey')}> Click me!</Button>
+}
+
+export default Button
